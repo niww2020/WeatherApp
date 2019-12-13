1330d7b2e04a1a4daf3898375ff62f79cecb74d0
niww <niww@ya.ru>
2019-12-14 00:07:57 +0300

    test SharedPreferences

--- app/src/main/java/com/example/weatherapp/ui/info/InfoFragment.java
+++ app/src/main/java/com/example/weatherapp/ui/info/InfoFragment.java
@@ -1,10 +1,12 @@
 package com.example.weatherapp.ui.info;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +17,8 @@ import com.example.weatherapp.ui.home.HomeViewModel;
 
 public class InfoFragment extends Fragment {
     Intent intent;
+    TextView linkToGit;
+
 
     private InfoViewModel infoViewModel;
 
@@ -24,7 +28,16 @@ public class InfoFragment extends Fragment {
                 ViewModelProviders.of(this).get(InfoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_info, container, false);
 
-//        intent = new Intent();
+        linkToGit = root.findViewById(R.id.linkToGitHub);
+
+        linkToGit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                intent = new Intent(getContext(),startActivity(Uri.parse(),););
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/niww1919/WeatherApp")));
+
+            }
+        });
 
         return root;
 
